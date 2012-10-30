e7a960a1ce1fbb39b0f31aa528db5c076fe32782
TheDekel <TheDekel@gmail.com>
2012-10-30 00:53:11 -0400

    successful retrieval of meme! need to talk to active tab...

--- back.js
+++ back.js
@@ -1,27 +1,60 @@
-var partialtext;
+var partialtext, memGen, imageGen;
 chrome.contextMenus.create({
   title: "Generate Meme",
   contexts:["selection", "editable"]
 });
 
+var getMemeGen = function(){
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		var res;
+		if (xhr.readyState === 4 && xhr.status === 200){
+			try {
+				debugger;
+				res = JSON.parse(xhr.responseText).result;
+				res = res[Math.floor(Math.random()*res.length)];
+				imageGen = /http:\/\/cdn\.memegenerator\.net\/images\/400x\/(\d*)\.jpg/.exec(res.imageUrl)[1];
+				memGen = res.generatorID;
+			} catch (e){
+				setTimeout(getMemeGen, 3000);
+			}
+		}
+	}
+	xhr.open("GET", "http://version1.api.memegenerator.net/Instances_Select_ByNew?languageCode=en&pageIndex=0&pageSize=3", true);
+	xhr.send();
+};
+getMemeGen();
 chrome.contextMenus.onClicked.addListener(function(e){
   var text = e.selectionText,
       sep = text.indexOf('\\'),
-      text1, text2;
+      text1, text2, xhr;
   console.log(e.selectionText);
   if (sep != -1 && text.length >= sep + 2 && text[sep + 1] != '\\'){
     text = text.split('\\');
     text1 = text[0];
     text2 = text[1];
-    debugger;
   } else if (partialtext){
     text1 = partialtext;
     text2 = text;
     partialtext = null;
-    debugger;
   } else {
     partialtext = text;
     return;
   }
-
+  xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(){
+    var res;
+	  if (xhr.readyState === 4 && xhr.status === 200){
+	    try {
+          res = JSON.parse(xhr.responseText).result;
+		  res = res.instanceImageUrl; 
+		  debugger;
+		} catch (e){
+		  return;
+		}
+	 }
+  };
+  xhr.open("GET","http://version1.api.memegenerator.net/Instance_Create?username=dekelgen&password=reindeerflotilla&languageCode=en&generatorID="+memGen+"&imageID="+imageGen+"&text0="+text1.replace(" ","%20")+"&text1="+text2.replace(" ","%20"), true);
+  xhr.send()
+  getMemeGen();
 });
